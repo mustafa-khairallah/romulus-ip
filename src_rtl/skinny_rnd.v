cf152fbbea0fa231ef66f5175c0ca997e3dc811e
Mustafa Khairallah <mustafam001@e.ntu.edu.sg>
2022-02-02 21:44:07 +0800

    Inserted skinny in the wrapper

--- src_rtl/skinny_rnd.v
+++ src_rtl/skinny_rnd.v
@@ -4,7 +4,7 @@ module skinny_rnd (/*AUTOARG*/
    // Inputs
    roundkey, roundtweak, roundstate, roundcnt, constant
    ) ;
-   parameter numrnd = 8;
+   parameter numrnd = 10;
    parameter fullcnt = 1;
 
    output [63+64*fullcnt:0] nextcnt;
