f5e7952a20c366d22da5cde6e887913851110acb
Mustafa Khairallah <mustafam001@e.ntu.edu.sg>
2022-02-02 17:40:07 +0800

    Initial Commit

---
+++ src_rtl/romulus_datapath.v
@@ -0,0 +1,247 @@
+module romulus_datapath (/*AUTOARG*/
+   // Outputs
+   pdo, counter,
+   // Inputs
+   constant, decrypt, pdi, sdi, domain, clk, srst, senc, sen, xrst, xenc, xen,
+   yrst, yenc, yen, zrst, zenc, zen, erst, correct_cnt, tk1s
+   ) ;
+   parameter buswidth = 128;
+   parameter constantwidth = 12;
+   parameter fullcnt = 0; // 1 if we want full 128 bit tweakey physically, 
+   // 0 for seleting half the key (useful for skinny)
+
+   output [buswidth-1:0] pdo;
+   output [55:0]         counter;
+
+   input [constantwidth-1:0] constant;
+   input [buswidth/8-1:0]    decrypt;
+   input [buswidth-1:0]      pdi;
+   input [buswidth-1:0]      sdi;
+   input [7:0]               domain;
+
+   input                     clk;
+   input                     srst, senc, sen;
+   input                     xrst, xenc, xen;
+   input                     yrst, yenc, yen;
+   input                     zrst, zenc, zen;
+   input                     erst;
+   input                     correct_cnt;
+   input                     tk1s;
+
+   wire [127:0]              state, key, tweak;
+   wire [127:0]              tbcstate;
+   wire [127:0]              tkxtbc, tkxcorrect;
+   wire [127:0]              tkytbc, tkycorrect;
+   wire [63+64*fullcnt:0]    tkztbc, tkzcorrect, domainseperator;
+   wire [127:0]              tka, tkb;
+   wire [63+64*fullcnt:0]    tkc;
+   wire [127:0]              tk1, tk2;
+   wire [63+64*fullcnt:0]    tk3, cin;
+
+   assign counter = domainseparator [63+64*fullcnt:8+64*fullcnt];
+   assign cin = correct_cnt ? domainseparator[63+64*fullcnt:8+64*fullcnt] : tkc[63+64*fullcnt:8+64*fullcnt];
+
+   state_update #(.buswidth(buswidth)) STATE (.pdo(pdo),
+                                              .state_o(state),
+                                              .decrypt(decrypt),
+                                              .pdi(pdi),
+                                              .state_i(tbcstate),
+                                              .clk(clk),
+                                              .rst(srst),
+                                              .en(sen),
+                                              .tbc(senc)
+                                              );
+
+   tkx_update #(.buswidth(buswidth)) TKEYX (.tkx(key),
+                                            .sdi(sdi),
+                                            .tkxtbc(tkxtbc),
+                                            .tkxcorrect(tkxcorrect),
+                                            .clk(clk),
+                                            .rst(xrst),
+                                            .tbc(xenc),
+                                            .en(xen)
+                                            ) ;
+
+   tkx_update #(.buswidth(buswidth)) TKEYY (.tkx(tweak),
+                                            .sdi(pdi),
+                                            .tkxtbc(tkytbc),
+                                            .tkxcorrect(tkycorrect),
+                                            .clk(clk),
+                                            .rst(yrst),
+                                            .tbc(yenc),
+                                            .en(yen)
+                                            ) ;
+
+   tkz_update #(.fullcnt(fullcnt)) TKEYZ (.tkz(domainseparator),
+                                          .tkztbc(tkztbc),
+                                          .tkzcorrect(tkzcorrect),
+                                          .clk(clk),
+                                          .rst(zrst),
+                                          .tbc(zenc),
+                                          .en(zen)
+                                          ) ;
+
+   correctfullperm PERMA (.tko(tka),.tki(key));
+   correctfullperm PERMB (.tko(tkb),.tki(tweak));
+   generate
+      if (fullcnt) begin: cnt_correction
+         correctfullperm PERMC (.tko(tkc),.tki(domainseperator));
+      end
+      else begin
+         correcthalfperm PERMC (.tko(tkc),.tki(domainseperator));
+      end
+   endgenerate
+
+   lfsr_counter #(.fullcnt(fullcnt)) CNT (.so(tk3),si(cin),.domain(domain));
+   lfsr2_correct LFSR3 (.so(tk1), .si(tka));
+   lfsr3_correct LFSR2 (.so(tk2), .si(tkb));
+
+   assign tkxcorrect = tk1;
+   assign tkycorrect = tk2;
+   assign tkzcorrect = tk3;
+
+   roundfunction #(.fullcnt(fullcnt)) tweakablecipher (.nextcnt(tkztbc),
+                                                       .nextkey(tkxtbc),
+                                                       .nexttweak(tkytbc),
+                                                       .nextstate(tbcstate),
+                                                       .roundkey(key),
+                                                       .roundtweak(tweak),
+                                                       .roundcnt(domainseparator),
+                                                       .roundstate(state),
+                                                       .constant(constant)
+                                                       );
+
+endmodule // romulus_datapath
+
+module state_update (/*AUTOARG*/
+   // Outputs
+   pdo, state_o,
+   // Inputs
+   decrypt, pdi, state_i, clk, rst, en, tbc
+   ) ;
+   parameter buswidth;
+
+   output [buswidth-1:0] pdo;
+   output [127:0]        state_o;
+
+   input [buswidth/8-1:0] decrypt;
+   input [buswidth-1:0]   pdi;
+   input [127:0]          state_i;
+   input                  clk, rst, en, tbc;
+
+   wire [buswidth-1:0]    pdi_eff;
+   wire [buswidth-1:0]    state_buf;
+   wire [buswidth-1:0]    gofs;
+   wire [127:0]           si;
+
+   reg [127:0]            state;
+
+   genvar                 i;
+
+   generate
+      for (i = 0; i < buswidth/8; i = i + 1) begin:decrypt_mux
+         assign pdi_eff[8*i+7:8*i] = decrypt[i] ? pdo[8*i+7:8*i] : pdi[8*i+7:8*i];
+      end
+   endgenerate
+
+   assign si = {state[128-buswidth-1:0],
+                pdi_eff^state[127:128-buswidth]};
+
+   assign state_buf = state[127:96-buswidth];
+
+   generate
+      for (i = 0; i < buswidth/8; i = i + 1) begin:gmatrix
+         assign gofs[8*i+7:8*i] = {state_buf[8*i+0]^state_buf[8*i+7],state_buf[8*i+7:8*i+1]};
+      end
+   endgenerate
+
+   assign pdo = pdi ^ gofs;
+
+   always @ (posedge clk) begin
+      if (rst) begin
+         state <= 0;
+      end
+      else begin
+         if (en) begin
+            if (tbc) begin
+               state <= state_i;
+            end
+            else begin
+               state <= si;
+            end
+         end
+      end
+   end // always @ (posedge clk)
+
+endmodule // state_update
+
+module tkx_update (/*AUTOARG*/
+   // Outputs
+   tkx,
+   // Inputs
+   sdi, tkxtbc, tkxcorrect, clk, rst, tbc, en
+   ) ;
+   parameter buswidth = 128;
+
+   output [127:0] tkx;
+
+   input [buswidth-1:0] sdi;
+   input [127:0]        tkxtbc, tkxcorrect;
+   input                clk, rst, tbc, en;
+
+   reg [127:0]          state;
+
+   always @ (posedge clk) begin
+      if (rst) begin
+         state <= {state[128-buswidth-1:0],sdi};
+      end
+      else if (en) begin
+         if (tbc) begin
+            state <= tkxtbc;
+         end
+         else begin
+            state <= tkxcorrect;
+         end
+      end
+   end
+
+endmodule // tkx_update
+
+module tkz_update (/*AUTOARG*/
+   // Outputs
+   tkz,
+   // Inputs
+   tkztbc, tkzcorrect, clk, rst, tbc, en
+   ) ;
+   parameter fullcnt = 0;
+
+   output [63+fullcnt*64:0] tkz;
+
+   input [63fullcnt*64:0]   tkztbc, tkzcorrect;
+   input                    clk, rst, tbc, en;
+
+   reg [63fullcnt*64:0]     state;
+
+   always @ (posedge clk) begin
+      if (rst) begin
+         generate
+              if (fullcnt == 1) begin:initial_counter_state
+              state <= 128'h01000000000000000000000000000000;
+           end
+           else begin
+              state <= 64'h0100000000000000;
+           end
+         endgenerate
+      end
+      else if (en) begin
+         if (tbc) begin
+            state <= tkztbc;
+         end
+         else begin
+            state <= tkzcorrect;
+         end
+      end
+   end
+
+endmodule // tkz_update
+
---
+++ src_rtl/skinny_rnd.v
@@ -0,0 +1,305 @@
+module skinny_rnd (/*AUTOARG*/ ) ;
+   parameter numrnd = 2;
+   parameter fullcnt = 1;
+
+   output [63+64*fullcnt:0] nextcnt;
+   output [127:0]           nextkey, nexttweak, nextstate;
+   input [127:0]           roundkey, roundtweak, roundstate;
+   input [63+64*fullcnt:0]  roundcnt;
+   input [5+6*(numrnd-1):0] constant;
+
+   genvar    i, j;
+
+   wire [63+64*fullcnt:0] rndcnt [numrnd:0];
+   wire [127:0] sb [numrnd-1:0];
+   wire [127:0] rkey [numrnd-1:0];
+   wire [127:0] atk [numrnd-1:0];
+   wire [127:0] shrow [numrnd-1:0];
+   wire [127:0] mxc [numrnd-1:0];
+   wire [127:0] rndkey [numrnd:0];
+   wire [127:0] rndtweak [numrnd:0];
+
+   wire [5:0]   rndconstant [numrnd-1:0];
+
+   generate
+      for (j = 0; j < 16; j = j + 1) begin:sbox_round0
+         skinny_sbox8 sbox0 (.so(sb[0][8*j+7:8*j]),
+                             .si(roundstate[8*j+7:8*j]));
+      end
+   endgenerate
+
+   // Add Tweakey
+   assign atk[0] = rkey[0] ^ sb[0];
+
+   // ShiftRows
+   assign shr[0][127:96] =  atk[0][127:96];
+   assign shr[0][ 95:64] = {atk[0][ 71:64],atk[0][95:72]};
+   assign shr[0][ 63:32] = {atk[0][ 47:32],atk[0][63:48]};
+   assign shr[0][ 31: 0] = {atk[0][ 23: 0],atk[0][31:24]};
+
+   // MixColumn
+   assign mxc[0][ 95:64] = shr[0][127:96];
+   assign mxc[0][ 63:32] = shr[0][ 95:64] ^ shr[0][63:32];
+   assign mxc[0][ 31: 0] = shr[0][127:96] ^ shr[0][63:32];
+   assign mxc[0][127:96] = shr[0][ 31: 0] ^ mxc[0][31: 0];
+
+   generate
+      for (i = 1; i < numrnd; i = i + 1) begin:unrolled_rounds
+         for (j = 0; j < 16; j = j + 1) begin:sbox_layer
+            generate
+               for (j = 0; j < 16; j = j + 1) begin:sbox_round0
+                  skinny_sbox8 sboxi (.so(sb[i][8*j+7:8*j]),
+                                      .si(mxc[i-1][8*j+7:8*j]));
+               end
+            endgenerate
+
+            // Add Tweakey
+            assign atk[i] = rkey[i] ^ sb[i];
+
+            // ShiftRows
+            assign shr[i][127:96] =  atk[i][127:96];
+            assign shr[i][ 95:64] = {atk[i][ 71:64],atk[i][95:72]};
+            assign shr[i][ 63:32] = {atk[i][ 47:32],atk[i][63:48]};
+            assign shr[i][ 31: 0] = {atk[i][ 23: 0],atk[i][31:24]};
+
+            // MixColumn
+            assign mxc[i][ 95:64] = shr[i][127:96];
+            assign mxc[i][ 63:32] = shr[i][ 95:64] ^ shr[i][63:32];
+            assign mxc[i][ 31: 0] = shr[i][127:96] ^ shr[i][63:32];
+            assign mxc[i][127:96] = shr[i][ 31: 0] ^ mxc[i][31: 0];
+         end
+      end
+   endgenerate
+
+   assign nextstate = mxc[numrnd-1];
+
+   assign rndkey[0] = roundkey;
+   assign rndtweak[0] = roundtweak;
+   assign rndcnt[0] = roundcnt;
+
+   generate
+      for (i = 0; i < numrnd; i = i + 1) begin:round_constant
+         assign rndconstant[i] = constant[5+6*i:0+6*i];
+      end
+   endgenerate
+
+   generate
+      for (i = 0; i < numrnd; i = i + 1) begin:keyexpansion
+         key_expansion tk3 (.ko(rndkey[i+1]),.ki(rndkey[i]));
+         tweak2_expansion tk2 (.ko(rndtweak[i+1]),.ki(rndtweak[i]));
+
+         if (fullcnt) begin: round_keys
+            tweak1_expansion #(.fullcnt(fullcnt)) tk1 (.ko(rndcnt[i+1]),.ki(rndcnt[i]));
+            assign rkey[i] = {rndkey[i][127:64],64'h0} ^
+                             {rndtweak[i][127:64],64'h0} ^
+                             {rndcnt[i][127:64],64'h0}^
+		                         {4'h0,rndcoonstant[i][3:0],24'h0,6'h0,rndcoonstant[i][5:4],24'h0,8'h02,56'h0};
+         end
+         else if (i%2 == 0) begin: even_round_keys
+            tweak1_expansion #(.fullcnt(fullcnt)) tk1 (.ko(rndcnt[i+2]),.ki(rndcnt[i]));
+            assign rkey[i] = {rndkey[i][127:64],64'h0} ^
+                             {rndtweak[i][127:64],64'h0} ^
+                             {rndcnt[i][127:64],64'h0}^
+		                         {4'h0,rndcoonstant[i][3:0],24'h0,6'h0,rndcoonstant[i][5:4],24'h0,8'h02,56'h0};
+         end
+         else begin: odd_round_keys
+            assign rndcnt[i] = 64'h0;
+            assign rkey[i] = {rndkey[i][127:64],64'h0} ^
+                             {rndtweak[i][127:64],64'h0} ^
+		                         {4'h0,rndcoonstant[i][3:0],24'h0,6'h0,rndcoonstant[i][5:4],24'h0,8'h02,56'h0};
+         end
+      end
+   endgenerate
+
+   assign nextkey = rndkey[numrnd];
+   assign nexttweak = rndtweak[numrnd];
+   assign nextcnt = rndcnt[numrnd];
+
+endmodule // skinny_rnd
+
+module key_expansion (/*AUTOARG*/
+   // Outputs
+   ko,
+   // Inputs
+   ki
+   ) ;
+   output [127:0] ko;
+   input  [127:0] ki;
+
+   wire [127:0]   kp;
+
+   assign kp[127:120] = ki[ 55: 48];
+   assign kp[119:112] = ki[  7:  0];
+   assign kp[111:104] = ki[ 63: 56];
+   assign kp[103: 96] = ki[ 23: 16];
+   assign kp[ 95: 88] = ki[ 47: 40];
+   assign kp[ 87: 80] = ki[ 15:  8];
+   assign kp[ 79: 72] = ki[ 31: 24];
+   assign kp[ 71: 64] = ki[ 39: 32];
+   assign kp[ 63: 56] = ki[127:120];
+   assign kp[ 55: 48] = ki[119:112];
+   assign kp[ 47: 40] = ki[111:104];
+   assign kp[ 39: 32] = ki[103: 96];
+   assign kp[ 31: 24] = ki[ 95: 88];
+   assign kp[ 23: 16] = ki[ 87: 80];
+   assign kp[ 15:  8] = ki[ 79: 72];
+   assign kp[  7:  0] = ki[ 71: 64];
+
+   assign ko[127:120] = {kp[120]^kp[126],kp[127:121]};
+   assign ko[119:112] = {kp[112]^kp[118],kp[119:113]};
+   assign ko[111:104] = {kp[104]^kp[110],kp[111:105]};
+   assign ko[103: 96] = {kp[ 96]^kp[102],kp[103: 97]};
+   assign ko[ 95: 88] = {kp[ 88]^kp[ 94],kp[ 95: 89]};
+   assign ko[ 87: 80] = {kp[ 80]^kp[ 86],kp[ 87: 81]};
+   assign ko[ 79: 72] = {kp[ 72]^kp[ 78],kp[ 79: 73]};
+   assign ko[ 71: 64] = {kp[ 64]^kp[ 70],kp[ 71: 65]};
+
+   assign ko[ 63:  0] = kp[ 63:  0];
+
+endmodule // key_expansion
+
+module tweak2_expansion (/*AUTOARG*/
+   // Outputs
+   ko,
+   // Inputs
+   ki
+   ) ;
+   output [127:0] ko;
+   input  [127:0] ki;
+
+   wire [127:0]   kp;
+
+   assign kp[127:120] = ki[ 55: 48];
+   assign kp[119:112] = ki[  7:  0];
+   assign kp[111:104] = ki[ 63: 56];
+   assign kp[103: 96] = ki[ 23: 16];
+   assign kp[ 95: 88] = ki[ 47: 40];
+   assign kp[ 87: 80] = ki[ 15:  8];
+   assign kp[ 79: 72] = ki[ 31: 24];
+   assign kp[ 71: 64] = ki[ 39: 32];
+   assign kp[ 63: 56] = ki[127:120];
+   assign kp[ 55: 48] = ki[119:112];
+   assign kp[ 47: 40] = ki[111:104];
+   assign kp[ 39: 32] = ki[103: 96];
+   assign kp[ 31: 24] = ki[ 95: 88];
+   assign kp[ 23: 16] = ki[ 87: 80];
+   assign kp[ 15:  8] = ki[ 79: 72];
+   assign kp[  7:  0] = ki[ 71: 64];
+
+   assign ko[127:120] = {kp[126:120],kp[127]^kp[125]};
+   assign ko[119:112] = {kp[118:112],kp[119]^kp[117]};
+   assign ko[111:104] = {kp[110:104],kp[111]^kp[109]};
+   assign ko[103: 96] = {kp[102: 96],kp[103]^kp[101]};
+   assign ko[ 95: 88] = {kp[ 94: 88],kp[ 95]^kp[ 93]};
+   assign ko[ 87: 80] = {kp[ 86: 80],kp[ 87]^kp[ 85]};
+   assign ko[ 79: 72] = {kp[ 78: 72],kp[ 79]^kp[ 77]};
+   assign ko[ 71: 64] = {kp[ 70: 64],kp[ 71]^kp[ 69]};
+
+   assign ko[ 63:  0] = kp[ 63:  0];
+
+endmodule // tweak2_expansion
+
+module tweak1_expansion (/*AUTOARG*/
+   // Outputs
+   ko,
+   // Inputs
+   ki
+   ) ;
+   parameter fullcnt = 0;
+
+   output [63+64*fullcnt:0] ko;
+   input [63+64*fullcnt:0]  ki;
+
+   wire [63+64*fullcnt:0]   kp;
+
+   generate
+      if (fullcnt) begin:full_size_counter
+         assign kp[127:120] = ki[ 55: 48];
+         assign kp[119:112] = ki[  7:  0];
+         assign kp[111:104] = ki[ 63: 56];
+         assign kp[103: 96] = ki[ 23: 16];
+         assign kp[ 95: 88] = ki[ 47: 40];
+         assign kp[ 87: 80] = ki[ 15:  8];
+         assign kp[ 79: 72] = ki[ 31: 24];
+         assign kp[ 71: 64] = ki[ 39: 32];
+         assign kp[ 63: 56] = ki[127:120];
+         assign kp[ 55: 48] = ki[119:112];
+         assign kp[ 47: 40] = ki[111:104];
+         assign kp[ 39: 32] = ki[103: 96];
+         assign kp[ 31: 24] = ki[ 95: 88];
+         assign kp[ 23: 16] = ki[ 87: 80];
+         assign kp[ 15:  8] = ki[ 79: 72];
+         assign kp[  7:  0] = ki[ 71: 64];
+      end // block: full_size_counter
+      else begin:half_size_counter
+         assign kp[63:56] = ki[ 55: 48];
+         assign kp[55:48] = ki[  7:  0];
+         assign kp[47:40] = ki[ 63: 56];
+         assign kp[39:32] = ki[ 23: 16];
+         assign kp[31:24] = ki[ 47: 40];
+         assign kp[23:16] = ki[ 15:  8];
+         assign kp[15: 8] = ki[ 31: 24];
+         assign kp[ 7: 0] = ki[ 39: 32];
+      end
+   endgenerate
+
+   assign ko = kp;
+
+endmodule // tweak1_expansion
+
+
+/*
+ Designer: Mustafa Khairallah
+ Nanyang Technological University
+ Singapore
+ Date: July, 2021
+ */
+
+/*
+ This file is the basic implementation of the logic-based Skinny Sbox8.
+ */
+
+module skinny_sbox8_logic (
+                     // Outputs
+                     so,
+                     // Inputs
+                     si
+                     ) ;
+   output [7:0] so;
+   input [7:0]  si;
+
+   wire [7:0]   a;
+
+   skinny_sbox8_cfn b764 (a[0],si[7],si[6],si[4]);
+   skinny_sbox8_cfn b320 (a[1],si[3],si[2],si[0]);
+   skinny_sbox8_cfn b216 (a[2],si[2],si[1],si[6]);
+   skinny_sbox8_cfn b015 (a[3], a[0], a[1],si[5]);
+   skinny_sbox8_cfn b131 (a[4], a[1],si[3],si[1]);
+   skinny_sbox8_cfn b237 (a[5], a[2], a[3],si[7]);
+   skinny_sbox8_cfn b303 (a[6], a[3], a[0],si[3]);
+   skinny_sbox8_cfn b452 (a[7], a[4], a[5],si[2]);
+
+   assign so[6] = a[0];
+   assign so[5] = a[1];
+   assign so[2] = a[2];
+   assign so[7] = a[3];
+   assign so[3] = a[4];
+   assign so[1] = a[5];
+   assign so[4] = a[6];
+   assign so[0] = a[7];
+
+endmodule // skinny_sbox8
+
+// The core repeated function (x nor y) xor z
+module skinny_sbox8_cfn (
+                         // Outputs
+                         f,
+                         // Inputs
+                         x, y, z
+                         ) ;
+   output f;
+   input  x, y, z;
+
+   assign f = ((~x) & (~y)) ^ z;
+
+endmodule // skinny_sbox8_cfn
